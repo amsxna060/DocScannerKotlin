354634e0f00c719b733221eec0e97921142bcda5
Antriksh1234 <59365244+Antriksh1234@users.noreply.github.com>
2020-10-09 09:18:42 +0530

    Remove error in launching of MainActivity.kt
    Initialized all buttons using lateinit var, converted their actions into property access syntax and opted for gradle update as recommended by Android studio

--- app/src/main/java/com/amansiol/docscanner/MainActivity.kt
+++ app/src/main/java/com/amansiol/docscanner/MainActivity.kt
@@ -11,62 +11,75 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
-//    private val fab_main=findViewById<FloatingActionButton>(R.id.fab)
-//    private val fab1_cam=findViewById<FloatingActionButton>(R.id.cam_fab)
-//    private val fab2_view=findViewById<FloatingActionButton>(R.id.fab_view)
-//    private val fab2_conversion=findViewById<FloatingActionButton>(R.id.fab_convert)
-//    private val fab_open=AnimationUtils.loadAnimation(applicationContext, R.anim.fab_open)
-//    private val fab_close= AnimationUtils.loadAnimation(applicationContext, R.anim.fab_close)
-//    private val fab_clock=AnimationUtils.loadAnimation(applicationContext, R.anim.rotate_fab_clock)
-//    private val fab_anticlock= AnimationUtils.loadAnimation(applicationContext, R.anim.fab_rotate_anticlock)
-//    var textview_view=findViewById<View>(R.id.textview_cam) as TextView
-//    val textview_cam= findViewById<View>(R.id.textview_view) as TextView
-//    val textview_convert= findViewById<View>(R.id.textView_convert) as TextView
-//    var isOpen =false
+
+    lateinit var fab_main: FloatingActionButton
+    lateinit var fab1_cam: FloatingActionButton
+    lateinit var fab2_view: FloatingActionButton
+    lateinit var fab_open: Animation
+    lateinit var fab_close: Animation
+    lateinit var fab_clock: Animation
+    lateinit var fab_anticlock: Animation
+    lateinit var fab2_conversion: FloatingActionButton
+    lateinit var textview_view: TextView
+    lateinit var textview_cam: TextView
+    lateinit var textview_convert: TextView
+    var isOpen =false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        fab_main.setOnClickListener(View.OnClickListener {
-//            if (isOpen) {
-//                textview_view.setVisibility(View.INVISIBLE)
-//                textview_cam.setVisibility(View.INVISIBLE)
-//                textview_convert.setVisibility(View.INVISIBLE)
-//                fab2_conversion.startAnimation(fab_close)
-//                fab2_view.startAnimation(fab_close)
-//                fab1_cam.startAnimation(fab_close)
-//                fab_main.startAnimation(fab_anticlock)
-//                fab2_view.setClickable(false)
-//                fab2_conversion.setClickable(false)
-//                fab1_cam.setClickable(false)
-//                isOpen = false
-//            } else {
-//                textview_view.setVisibility(View.VISIBLE)
-//                textview_cam.setVisibility(View.VISIBLE)
-//                textview_convert.setVisibility(View.VISIBLE)
-//                fab2_conversion.startAnimation(fab_open)
-//                fab2_view.startAnimation(fab_open)
-//                fab1_cam.startAnimation(fab_open)
-//                fab_main.startAnimation(fab_clock)
-//                fab2_view.setClickable(true)
-//                fab2_conversion.setClickable(true)
-//                fab1_cam.setClickable(true)
-//                isOpen = true
-//            }
-//        })
+        fab_main = findViewById(R.id.fab)
+        fab1_cam = findViewById(R.id.cam_fab)
+        fab2_view = findViewById(R.id.fab_view)
+        fab2_conversion = findViewById(R.id.fab_convert)
+        fab_open =AnimationUtils.loadAnimation(applicationContext, R.anim.fab_open)
+        fab_close = AnimationUtils.loadAnimation(applicationContext, R.anim.fab_close)
+        fab_clock = AnimationUtils.loadAnimation(applicationContext, R.anim.rotate_fab_clock)
+        fab_anticlock = AnimationUtils.loadAnimation(applicationContext, R.anim.fab_rotate_anticlock)
+        textview_view = findViewById(R.id.textview_cam)
+        textview_cam = findViewById(R.id.textview_view)
+        textview_convert= findViewById(R.id.textView_convert)
 
+        fab_main.setOnClickListener(View.OnClickListener {
+            if (isOpen) {
+                textview_view.visibility = View.INVISIBLE
+                textview_cam.visibility = View.INVISIBLE
+                textview_convert.visibility = View.INVISIBLE
+                fab2_conversion.startAnimation(fab_close)
+                fab2_view.startAnimation(fab_close)
+                fab1_cam.startAnimation(fab_close)
+                fab_main.startAnimation(fab_anticlock)
+                fab2_view.isClickable = false
+                fab2_conversion.isClickable = false
+                fab1_cam.isClickable = false
+                isOpen = false
+            } else {
+                textview_view.visibility = View.VISIBLE
+                textview_cam.visibility = View.VISIBLE
+                textview_convert.visibility = View.VISIBLE
+                fab2_conversion.startAnimation(fab_open)
+                fab2_view.startAnimation(fab_open)
+                fab1_cam.startAnimation(fab_open)
+                fab_main.startAnimation(fab_clock)
+                fab2_view.isClickable = true
+                fab2_conversion.isClickable = true
+                fab1_cam.setClickable(true)
+                isOpen = true
+            }
+        })
 
-//        fab2_view.setOnClickListener(View.OnClickListener {
-//            Toast.makeText(this,"view pdfs",Toast.LENGTH_LONG);
-//        })
-//
-//        fab1_cam.setOnClickListener(View.OnClickListener {
-//            Toast.makeText(this,"camera",Toast.LENGTH_LONG);
-//
-//        })
-//        fab2_conversion.setOnClickListener(View.OnClickListener {
-//            Toast.makeText(this,"Convert",Toast.LENGTH_LONG);
-//        })
+        fab2_view.setOnClickListener(View.OnClickListener {
+            Toast.makeText(this,"view pdfs",Toast.LENGTH_LONG).show()
+        })
+
+        fab1_cam.setOnClickListener(View.OnClickListener {
+            Toast.makeText(this,"camera",Toast.LENGTH_LONG).show()
+        })
+
+        fab2_conversion.setOnClickListener(View.OnClickListener {
+            Toast.makeText(this,"Convert",Toast.LENGTH_LONG).show()
+        })
 
     }
 }
